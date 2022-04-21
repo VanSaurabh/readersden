d5bb3e369fd29c83c4bc105d0dd0c18bce977d0d
Saurabh Kundu <skundu@mobiquityinc.com>
2022-04-21 14:29:07 +0200

    add/delete db operation

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import uuid
 def main():
     client = get_client()
     add_data_to_db(client)
+    delete_data_from_db(client)
 
 
 def add_data_to_db(client):
@@ -23,9 +24,14 @@ def add_data_to_db(client):
     )
 
 
+def delete_data_from_db(client):
+    collection_name = "demo_book"
+    client.request(http_methods.DELETE, path=f"/api/rest/v2/namespaces/{astra_db_keyspace}/collections/{collection_name}/{id}")
+
+
 def get_client():
     return create_client(astra_database_id=astra_db_id, astra_database_region=astra_db_region,
-                  astra_application_token=astra_db_application_token)
+                         astra_application_token=astra_db_application_token)
 
 
 if __name__ == "__main__":
