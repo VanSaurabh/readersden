6d5eed5d28943349d51c84f577549e170b9c1b41
Saurabh Kundu <skundu@mobiquityinc.com>
2022-04-21 14:08:57 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # readersden
 a website to track your book reading
+
+
+#### install python
+
+```sh
+brew install python
+```
+
+### check python version
+
+```sh
+python3 --version
+```
+
+### pip version
+
+```sh
+pip3 -v
+```
+
+### install libraries
+
+```sh
+pip3 install -r requirements.txt
+```
---
+++ src/main.py
@@ -0,0 +1,32 @@
+from astrapy.rest import create_client, http_methods
+from util import astra_db_id, astra_db_keyspace, astra_db_region, astra_db_application_token
+import uuid
+
+
+def main():
+    client = get_client()
+    add_data_to_db(client)
+
+
+def add_data_to_db(client):
+    collection_name = "demo_book"
+    doc_uuid = uuid.uuid4()
+
+    client.request(
+        http_methods.PUT,
+        path=f"/api/rest/v2/namespaces/{astra_db_keyspace}/collections/{collection_name}/{doc_uuid}",
+        json_data={
+            "book": "The Hunger Games1",
+            "author": "Suzanne Collins1",
+            "genre": ["fiction"],
+        }
+    )
+
+
+def get_client():
+    return create_client(astra_database_id=astra_db_id, astra_database_region=astra_db_region,
+                  astra_application_token=astra_db_application_token)
+
+
+if __name__ == "__main__":
+    main()
